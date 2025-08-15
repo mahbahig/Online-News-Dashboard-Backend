3ec0db39322a00555b76e65500ee8136356aa25c
mahbahig <mahbahig2@gmail.com>
2025-08-15 21:10:16 +0300

    Used authentication middleware in the user router

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import * as UC from "./user.controller.js";
 import * as UV from "./user.validator.js";
 import { validation } from "../../middleware/validation.js";
+import { authentication } from "../../middleware/authentication.js";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validation(UV.signupSchema), UC.signup);
 userRouter.post("/login", validation(UV.loginSchema), UC.login);
-userRouter.get("/:id",  UC.getUser);
+userRouter.get("/:id", authentication, UC.getUser);
 
 
 export default userRouter;
